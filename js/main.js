05a8814ead96a70b79ce578b7b10d2e656cdfcce
Jonathan Fekadu <jonathanfekadu1@gmail.com>
2024-06-10 06:12:11 +0300

    committed

--- js/main.js
+++ js/main.js
@@ -22,5 +22,10 @@ const sideBarElements = document.querySelector(".side-bar-elements");
 dropIcon.onclick = () => {
   dropIcon.classList.toggle("active");
   sideBarElements.classList.toggle("active");
-  body.classList.toggle("covered");
+
+  if (dropIcon.classList.value === "drop-icon active") {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "auto";
+  }
 };
